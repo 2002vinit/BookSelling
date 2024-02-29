997b328c4634b5a377c0ee5a7b2d55357398153d
2002vinit <158330761+2002vinit@users.noreply.github.com>
2024-02-29 22:10:50 +0530

    Add files via upload

---
+++ contactUs.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    
+  </head>
+  <body class="ContactUs_body">
+     <p>
+       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus non quo repellat possimus, fugit quia quidem illo ipsa atque? Sed quaerat quisquam cumque, autem odio nobis facilis incidunt veritatis quidem cupiditate iste minima beatae temporibus corrupti fuga dolorum, enim quia vero? Repudiandae, iusto. Eum veniam sint, molestiae, doloremque qui maiores mollitia reiciendis quis enim adipisci odit expedita culpa ipsam porro iste laudantium voluptatum praesentium obcaecati fuga quibusdam sunt hic? Laborum eligendi beatae harum odit suscipit, molestias accusamus numquam. Voluptatibus velit quae natus placeat vel sit laboriosam, magni illo, dicta eaque repellendus assumenda quia fuga illum labore quam facilis nostrum mollitia corporis ratione? Voluptates dolore atque rem maxime id laboriosam exercitationem, esse dignissimos hic deserunt ab quia, eius commodi. Ut itaque a nobis officia minus sit aut ducimus nulla, illo reprehenderit, repellat, velit fugit magnam consequatur officiis provident eius possimus. Quo cum quibusdam nesciunt animi delectus dolorum quasi nobis voluptas minus necessitatibus iste, iure nemo debitis nisi autem ab excepturi ullam voluptatibus sapiente nulla odio. Blanditiis aliquid vel natus esse voluptate delectus autem dolorum tenetur quibusdam ab quod rem aperiam, doloremque laborum minus eius rerum magni. Molestiae earum, perspiciatis harum modi vel unde amet officia itaque ipsam eveniet quia, labore quidem.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nobis excepturi, labore sunt quos consequatur natus voluptate, soluta minima cupiditate ipsum porro. Dolorem enim ullam minus et animi atque eius?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus totam ad dicta animi tempore! Ad aut aliquid dolores cum ex placeat, beatae, reiciendis ea praesentium doloribus sunt ipsam impedit? A ex quae explicabo recusandae expedita eos assumenda culpa perferendis sed enim voluptatem beatae officiis, dolore similique saepe iste magnam sit veritatis. Facere, inventore iste debitis dolores possimus, quibusdam minus exercitationem asperiores sapiente explicabo maxime minima omnis impedit? Ea fugit deleniti itaque, ab fugiat at culpa reprehenderit deserunt eius praesentium optio. Cumque debitis quas, quo voluptatibus inventore nesciunt exercitationem itaque ex et unde facere optio molestias repellendus minus excepturi facilis, nostrum a corporis placeat repudiandae cupiditate ipsa? Hic sunt minima error officiis accusantium veniam nihil quia vitae laboriosam consequatur velit magnam, aliquam sequi, earum rem quae. Expedita reprehenderit dolorem nisi hic dolores, blanditiis delectus fugiat. Sint, blanditiis! Asperiores placeat, totam laboriosam perspiciatis beatae neque eius deserunt expedita, repudiandae ea ipsa? Sint amet quidem molestiae deserunt sed! Voluptate nostrum aut placeat commodi dolorem eveniet distinctio. Corporis suscipit harum, adipisci quisquam amet nesciunt iste quibusdam libero tempora dolore expedita nihil nobis eos ad laborum doloribus voluptate molestias? Repellendus voluptatibus odit nesciunt, maiores consequuntur facere error officiis cumque ratione omnis itaque totam voluptatum at adipisci molestiae ut corporis labore aliquam iure ab, unde quod. Aliquam voluptatum eaque nobis sapiente blanditiis perspiciatis quisquam cumque culpa quidem tempore quos adipisci dolores illo pariatur, repudiandae explicabo repellendus alias doloribus! Id, nisi. Laboriosam consectetur ut suscipit mollitia totam minima dolorum aperiam neque iure molestias inventore, aliquid eum voluptatum vero illum, saepe incidunt at quis tempore accusamus dolorem ipsam? Repellendus quaerat vel, quo pariatur voluptatum eveniet nam necessitatibus in suscipit quos voluptate error iusto a, neque quia fugiat debitis. Laudantium, molestias. Non eligendi rerum quo dicta atque odit blanditiis dolore, quis, nobis autem tempore debitis optio nulla et alias? Deserunt, et vero quos nobis non optio quaerat provident. Voluptates cumque qui sunt, laudantium, fuga sint ipsum voluptatum sapiente hic, quibusdam minima asperiores? Laboriosam recusandae officia fugit placeat inventore vero veritatis omnis quod quas ipsum, eligendi eius nesciunt in aspernatur quos at corporis odit cumque ad, consectetur, eos accusantium numquam natus ut. Commodi eum, cumque sunt ex corrupti expedita rem voluptas rerum quae libero ea velit fugit possimus ut nemo hic placeat. Fugiat minima molestias laudantium alias modi recusandae, possimus eaque beatae in velit est harum mollitia, exercitationem, obcaecati maiores deleniti nihil excepturi laboriosam officia dolor ullam. Fugit, et ab?
+       
+    
+       <br>@7703931738
+     </p>
+    
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
